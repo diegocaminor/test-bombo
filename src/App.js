77d11d3b4d3c5edd6435ce0899a7f9e441f657f3
Diego Camino <diego.camino.r@gmail.com>
2020-07-16 00:30:27 -0500

    Add Register form and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
+import Register from "./containers/Register";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Register />
     </div>
   );
 }
---
+++ src/containers/Register.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+import "../assets/styles/components/Register.css";
+
+const Register = (props) => {
+  const [form, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    birthdate: "",
+    password: "",
+  });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+    alert(`Información enviada: ${JSON.stringify(form)}`);
+  };
+
+  return (
+    <>
+      <section className="register">
+        <section className="register__container">
+          <h2 className="register__title">Regístrate</h2>
+          <form className="register__container--form" onSubmit={handleSubmit}>
+            <input
+              name="firstName"
+              type="text"
+              className="input"
+              placeholder="Nombre"
+              onChange={handleInput}
+              required
+            />
+            <br />
+            <input
+              name="lastName"
+              type="text"
+              className="input"
+              placeholder="Apellidos"
+              onChange={handleInput}
+              required
+            />
+            <br />
+            <input
+              name="birthdate"
+              type="date"
+              className="input"
+              placeholder="Fecha nacimiento"
+              onChange={handleInput}
+              required
+            />
+            <br />
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Contraseña"
+              onChange={handleInput}
+              required
+            />
+            <br />
+            <button type="submit" className="button">
+              Enviar form
+            </button>
+          </form>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Register;
